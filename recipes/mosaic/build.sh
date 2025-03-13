cc90d9e9ec8e20899f4b3d57af0b087dea4d15e4
Christian Bator <christian@bator.co>
2025-03-13 15:33:03 +0100

    Add Mosaic

---
+++ recipes/mosaic/build.sh
@@ -0,0 +1,40 @@
+#
+# build.sh
+# mosaic
+#
+# Created by Christian Bator on 01/02/2025
+#
+
+set -euo pipefail
+
+#
+# Locations
+#
+mkdir -p $PREFIX/lib/mojo
+mkdir -p $PREFIX/lib/mosaic
+
+#
+# Build libcodec
+#
+cp external/stb/{stb_image.h,stb_image_write.h} libcodec
+
+if [[ -z "${OSX_ARCH+x}" ]]; then
+    clang -fPIC -shared -Wall -Werror -o $PREFIX/lib/mosaic/libcodec.dylib libcodec/libcodec.c
+else
+    clang -fPIC -shared -Wall -Werror -DSTBI_NEON -o $PREFIX/lib/mosaic/libcodec.dylib libcodec/libcodec.c
+fi
+
+#
+# Build libvisualizer
+#
+if [[ -z "${OSX_ARCH+x}" ]]; then
+    echo "> Unsupported platform for libvisualizer: $(uname)"
+else
+    swift_source_files=$(find libvisualizer/mac/MacVisualizer -name "*.swift")
+    swiftc -emit-library -o $PREFIX/lib/mosaic/libvisualizer.dylib $swift_source_files
+fi
+
+#
+# Build mosaic
+#
+mojo package mosaic -o $PREFIX/lib/mojo/mosaic.mojopkg
